73b58f85df931b0f3e94995d54fa1609033149c7
Laila Ali <laila@teambrightly.com>
2021-02-10 23:17:45 -0600

    Modify trailer function to work

--- src/components/Row/Row.tsx
+++ src/components/Row/Row.tsx
@@ -3,7 +3,8 @@ import css from "./Row.module.scss";
 import classes from "classnames";
 import axios from "../../axios";
 import YouTube from "react-youtube";
-// import movieTrailer from 'movie-trailer';
+
+const movieTrailer = require("movie-trailer");
 
 interface IRow {
    title: string;
@@ -17,14 +18,14 @@ const opts: any = {
    height: "390",
    width: "100%",
    playerVars: {
-      autoplay: 0,
+      autoplay: 1,
    },
 };
 
 export const Row: React.FC<IRow> = (props) => {
    const { title, fetchUrl, isRowLarge } = props;
    const [movies, setMovies] = useState([]);
-   const [trailerUrl, setTrailerUrl] = useState<string | null>(null);
+   const [trailerUrl, setTrailerUrl] = useState<any>("");
    useEffect(() => {
       async function fetchData() {
          const request = await axios.get(fetchUrl);
@@ -34,18 +35,19 @@ export const Row: React.FC<IRow> = (props) => {
       fetchData();
    }, []);
 
-   // const handleClick = (movie: any) => {
-   //    if (trailerUrl) {
-   //       setTrailerUrl(null);
-   //    } else {
-   //       movieTrailer(movie?.name || null)
-   //          .then((url: string) => {
-   //             const urlParams = new URLSearchParams(new URL(url).search);
-   //             setTrailerUrl(urlParams.get("v"));
-   //          })
-   //          .catch((error: string) => console.log(error));
-   //    }
-   // };
+   const handleClick = (movie: any) => {
+      if (trailerUrl) {
+         setTrailerUrl("");
+      } else {
+         movieTrailer(movie?.title || "")
+            .then((url: any) => {
+               const urlParams = new URLSearchParams(new URL(url).search);
+               setTrailerUrl(urlParams.get("v"));
+            })
+            .catch((error: string) => console.log(error));
+         console.log(trailerUrl);
+      }
+   };
 
    return (
       <div className={css.row}>
@@ -64,7 +66,7 @@ export const Row: React.FC<IRow> = (props) => {
                            src={`${base_url}${movie.poster_path}`}
                            alt={movie.name}
                            className={css.posterImage}
-                           // onClick={() => handleClick(movie)}
+                           onClick={() => handleClick(movie)}
                         />
                      </div>
                   </div>
